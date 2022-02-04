9ee587258f54fec05071e2d896cde4d6855daeaf
keisuke-333 <keikubo.999@gmail.com>
2022-02-04 13:25:01 +0900

    htmlファイル

--- practice.go
+++ practice.go
@@ -6,15 +6,10 @@ import "net/http"
 
 func main() {
 	engine := gin.Default()
-	ua := ""
-	engine.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
-		c.Next()
-	})
+	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "hello world",
-			"User-Agent": ua,
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"message": "hello gin",
 		})
 	})
 	engine.Run(":3000")
