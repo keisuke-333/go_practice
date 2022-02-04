ca01424d953e1865be52f120ae5b6948b147371a
keisuke-333 <keikubo.999@gmail.com>
2022-02-04 13:40:20 +0900

    静的ファイル

--- practice.go
+++ practice.go
@@ -2,15 +2,8 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-import "net/http"
-
 func main() {
 	engine := gin.Default()
-	engine.LoadHTMLGlob("templates/*")
-	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"message": "hello gin",
-		})
-	})
+	engine.Static("/static", "./static")
 	engine.Run(":3000")
 }
