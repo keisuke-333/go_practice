71bbe970116d144b991ce34f9d4d4da9e883a895
keisuke-333 <keikubo.999@gmail.com>
2022-02-04 15:20:18 +0900

    ポインタ

--- practice.go
+++ practice.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "fmt"
 
 func main() {
-	engine := gin.Default()
-	engine.Static("/static", "./static")
-	engine.Run(":3000")
+	var i int = 100
+	var j int = 200
+	var p1 *int
+	var p2 *int
+	p1 = &i
+	p2 = &j
+	i = *p1 + *p2
+	p2 = p1
+	j = *p2 + i
+	fmt.Println(j)
 }
