0c480232038bbeb827e3b6a6561873ea781f58c6
keisuke-333 <keikubo.999@gmail.com>
2022-02-07 11:19:14 +0900

    入力フォームのテキストを表示

--- practice.go
+++ practice.go
@@ -1,16 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func main() {
-	var i int = 100
-	var j int = 200
-	var p1 *int
-	var p2 *int
-	p1 = &i
-	p2 = &j
-	i = *p1 + *p2
-	p2 = p1
-	j = *p2 + i
-	fmt.Println(j)
+	router := gin.Default()
+
+	// 自動的にファイルを返すよう設定 --- (*1)
+	router.StaticFS("/static", http.Dir("static"))
+
+	// ルートなら /static/index.html にリダイレクト --- (*2)
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(302, "/static/index.html")
+	})
+
+	// フォームの内容を受け取って挨拶する --- (*3)
+	router.GET("/hello", func(ctx *gin.Context) {
+		name := ctx.Query("name")
+		ctx.Header("Content-Type", "text/html; charset=UTF-8")
+		ctx.String(200, "<h1>Hello, "+name+"</h1>")
+	})
+
+	// サーバーを起動
+	err := router.Run("127.0.0.1:8888")
+	if err != nil {
+		log.Fatal("サーバー起動に失敗", err)
+	}
 }
