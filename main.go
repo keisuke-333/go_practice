b014fefdd610417950dea6fada5d4685e13c0991
keisuke-333 <keikubo.999@gmail.com>
2022-02-08 11:11:22 +0900

    メソッド使い方

--- main.go
+++ main.go
@@ -1,28 +1,18 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
-type Person struct {
-	Age     int    `form:"age" validate:"required,gt=10,max=100"`
-	Name    string `form:"name" validate:"required"`
-	Address string `form:"address" validate:"required"`
+type Vertex struct {
+	X, Y int
+}
+
+func (v *Vertex) Plus() int {
+	return v.X + v.Y
 }
 
 func main() {
-	serve := gin.Default()
-
-	serve.GET("/testing", func(c *gin.Context) {
-		var person Person
-		if err := c.ShouldBind(&person); err != nil {
-			//c.String(500, "%v",err)
-			c.JSON(500, gin.H{"msg": err.Error()})
-			return
-		}
-		//c.String(200, "%v", person)
-		c.JSON(200, gin.H{"person": person})
-	})
-
-	serve.Run(":3000")
+	v := Vertex{3, 4}
+	fmt.Println(v.Plus())
 }
