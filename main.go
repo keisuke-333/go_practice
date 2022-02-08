2255626579cc5519f6ff9085e8985a12507b8f15
keisuke-333 <keikubo.999@gmail.com>
2022-02-08 18:21:02 +0900

    Gin基本

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/keisuke-333/go_practice/handler"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// インスタンス生成
 	r := gin.Default()
-
-	// プロジェクト内のhtmlを利用する
-	r.LoadHTMLGlob("templates/*")
-
-	// URLと処理の結び付け
-	r.GET("/user/:id", handler.GetUser)
-
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.Run(":3000")
 }
