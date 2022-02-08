183cc26fb4a1e0ee5596da993ea1b35a41158248
keisuke-333 <keikubo.999@gmail.com>
2022-02-08 11:18:19 +0900

    Stringer使い方

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ type Vertex struct {
 	X, Y int
 }
 
-func (v *Vertex) Plus() int {
-	return v.X + v.Y
+func (v Vertex) String() string {
+	return fmt.Sprintf("X is %d! Y is %d", v.X, v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v.Plus())
+	fmt.Println(v)
 }
