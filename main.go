f218360f0482c25bb4d3459d009389f569c481af
keisuke-333 <keikubo.999@gmail.com>
2022-02-09 11:53:55 +0900

    goroutine確認

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "fmt"
+
+func goroutine(s []string, c chan string) {
+	sum := ""
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":3000")
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c {
+		fmt.Println(w)
+	}
 }
