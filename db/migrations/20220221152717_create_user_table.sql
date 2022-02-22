80635e66bffa9f2233975a7558db26fafefab4fe
keisuke-333 <keikubo.999@gmail.com>
2022-02-22 10:17:24 +0900

    ginでHelloWorld

--- db/migrations/20220221152717_create_user_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE users
-(
-  id   int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255) NOT NULL
-);
-
--- +goose Down
-DROP TABLE users;
--- main.go
+++ main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
-	"log"
-	"os"
+	"net/http"
 
-	"github.com/joho/godotenv"
-	"github.com/keisuke-333/go_practice/entity"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// ————— 環境変数の読み込み —————
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	// —————————————————————————
+	r := gin.Default()
 
-	// ————— MySQL接続 —————
-	dns := os.Getenv("CONNECT")
-	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		sqlDB, err := db.DB()
-		if err == nil {
-			sqlDB.Close()
-		}
-	}()
-	// —————————————————————————
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world")
+	})
 
-	// ————— SQLログ出力 —————
-	db.Logger = db.Logger.LogMode(4)
-	// —————————————————————————
-
-	// ————— テーブル作成 —————
-	db.AutoMigrate(
-		&entity.User{},
-	)
-	// —————————————————————————
+	r.Run(":8080")
 }
