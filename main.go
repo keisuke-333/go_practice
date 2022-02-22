e0aab0c4bcc22d6b55c168a1b2388f3a5b89ea71
keisuke-333 <keikubo.999@gmail.com>
2022-02-22 12:04:24 +0900

    GormのAutoMigrationでテーブル作成

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"net/http"
+	"log"
+	// "net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/keisuke-333/go_practice/entity"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	r := gin.Default()
+	// ————— 環境変数の読み込み —————
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// —————————————————————————
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world")
-	})
+	// ————— MySQL接続 —————
+	dns := os.Getenv("CONNECT")
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		sqlDB, err := db.DB()
+		if err == nil {
+			sqlDB.Close()
+		}
+	}()
+	// —————————————————————————
 
-	r.Run(":8080")
+	// ————— SQLログ出力 —————
+	db.Logger = db.Logger.LogMode(4)
+	// —————————————————————————
+
+	// ————— テーブル作成 —————
+	db.AutoMigrate(
+		&entity.User{},
+	)
+	// —————————————————————————
 }
