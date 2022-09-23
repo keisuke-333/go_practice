6bf25a7227b6071b3b3309a9225ace599fff470d
keisuke-333 <keikubo.999@gmail.com>
2022-09-23 16:27:04 +0900

    byte

--- basic/main.go
+++ basic/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	// byte
+	b1 := []byte("a")
+	b2 := []byte("あ")
+	b3 := string(b1)
+	b4 := string(b2)
+	fmt.Println(b1, b2, b3, b4)
+	b5 := []byte{227, 129, 130}
+	fmt.Println(string(b5))
 }
