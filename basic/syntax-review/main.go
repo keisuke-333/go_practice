e4d734290b0460d0b29d8c5ada4619bc60a09f6d
keisuke-333 <keikubo.999@gmail.com>
2023-04-08 00:01:08 +0900

    pointer

--- basic/syntax-review/main.go
+++ basic/syntax-review/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"syntax-review/calculator"
+	"unsafe"
 
 	"github.com/joho/godotenv"
 )
@@ -42,4 +43,24 @@ func main() {
 	fmt.Printf("val: %v, type: %T\n", val5, val5)
 
 	fmt.Printf("Mac: %v, Windows: %v, Linux: %v\n", Mac, Windows, Linux)
+
+	// pointer
+	var ui1 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+	var p1 *uint16
+	fmt.Printf("value of p1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[bytes]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("value of ui1(dereference) %v\n", *p1)
+	*p1 = 1
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %v\n", pp1)
+	fmt.Printf("value of p1(dereference) %v\n", *pp1)
+	fmt.Printf("value of ui1(dereference) %v\n", **pp1)
+	**pp1 = 10
+	fmt.Printf("value of ui1: %v\n", ui1)
 }
