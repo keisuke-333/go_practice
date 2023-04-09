f30cd53010d93993a754e5b4bff917c922ba0171
keisuke-333 <keikubo.999@gmail.com>
2023-04-10 00:01:14 +0900

    array

--- basic/syntax-review/main.go
+++ basic/syntax-review/main.go
@@ -76,4 +76,12 @@ func main() {
 		println(result)
 	}
 	println(result)
+
+	// array
+	var arr1 [3]int
+	var arr2 = [3]int{10, 20, 30}
+	arr3 := [...]int{10, 20}
+	fmt.Printf("%v %v %v\n", arr1, arr2, arr3)
+	fmt.Printf("%v %v\n", len(arr3), cap(arr3))
+	fmt.Printf("%T %T\n", arr2, arr3)
 }
