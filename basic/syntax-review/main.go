ded7d7c603cf198acc879b2b39cff304d75fab74
keisuke-333 <keikubo.999@gmail.com>
2023-04-11 22:35:39 +0900

    slice

--- basic/syntax-review/main.go
+++ basic/syntax-review/main.go
@@ -84,4 +84,52 @@ func main() {
 	fmt.Printf("%v %v %v\n", arr1, arr2, arr3)
 	fmt.Printf("%v %v\n", len(arr3), cap(arr3))
 	fmt.Printf("%T %T\n", arr2, arr3)
+
+	// slice
+	var sl1 []int
+	sl2 := []int{}
+	fmt.Printf("sl1: %[1]T %[1]v %v %v\n", sl1, len(sl1), cap(sl1))
+	fmt.Printf("sl2: %[1]T %[1]v %v %v\n", sl2, len(sl2), cap(sl2))
+	fmt.Println(sl1 == nil)
+	fmt.Println(sl2 == nil)
+	sl1 = append(sl1, 1, 2, 3)
+	fmt.Printf("sl1: %[1]T %[1]v %v %v\n", sl1, len(sl1), cap(sl1))
+	sl3 := []int{4, 5, 6}
+	sl1 = append(sl1, sl3...)
+	fmt.Printf("sl1: %[1]T %[1]v %v %v\n", sl1, len(sl1), cap(sl1))
+
+	sl4 := make([]int, 0, 2)
+	fmt.Printf("sl4: %[1]T %[1]v %v %v\n", sl4, len(sl4), cap(sl4))
+	sl4 = append(sl4, 1, 2, 3, 4)
+	fmt.Printf("sl4: %[1]T %[1]v %v %v\n", sl4, len(sl4), cap(sl4))
+	sl5 := make([]int, 4, 6)
+	fmt.Printf("sl5: %[1]T %[1]v %v %v\n", sl5, len(sl5), cap(sl5))
+	sl6 := sl5[1:3]
+	sl6[1] = 10
+	fmt.Printf("sl5: %[1]T %[1]v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("sl6: %[1]T %[1]v %v %v\n", sl6, len(sl6), cap(sl6))
+	sl6 = append(sl6, 2)
+	fmt.Printf("sl5: %[1]T %[1]v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("sl6: %[1]T %[1]v %v %v\n", sl6, len(sl6), cap(sl6))
+	sc6 := make([]int, len(sl5[1:3]))
+	fmt.Printf("sl5 source of copy: %v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("sl6 dst copy before: %v %v %v\n", sc6, len(sc6), cap(sc6))
+	copy(sc6, sl5[1:3])
+	fmt.Printf("sl6 dst copy after: %v %v %v\n", sc6, len(sc6), cap(sc6))
+	sc6[1] = 12
+	fmt.Printf("sl5: %v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("sc6: %v %v %v\n", sc6, len(sc6), cap(sc6))
+
+	sl5 = make([]int, 4, 6)
+	fs6 := sl5[1:3:3]
+	fmt.Printf("sl5: %v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("fs6: %v %v %v\n", fs6, len(fs6), cap(fs6))
+	fs6[0] = 6
+	fs6[1] = 7
+	fs6 = append(fs6, 8)
+	fmt.Printf("sl5: %v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("fs6: %v %v %v\n", fs6, len(fs6), cap(fs6))
+	sl5[3] = 9
+	fmt.Printf("sl5: %v %v %v\n", sl5, len(sl5), cap(sl5))
+	fmt.Printf("fs6: %v %v %v\n", fs6, len(fs6), cap(fs6))
 }
