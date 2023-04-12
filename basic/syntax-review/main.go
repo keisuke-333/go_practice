728aeb433ccac8b9311d5cd25a46322fe474a0d9
keisuke-333 <keikubo.999@gmail.com>
2023-04-13 00:22:14 +0900

    struct

--- basic/syntax-review/main.go
+++ basic/syntax-review/main.go
@@ -15,6 +15,19 @@ const (
 	Linux
 )
 
+type Task struct {
+	Title    string
+	Estimate int
+}
+
+// receiver
+func (task Task) extendEstimate() {
+	task.Estimate += 10
+}
+func (taskp *Task) extendEstimatePointer() {
+	taskp.Estimate += 10
+}
+
 func main() {
 	godotenv.Load()
 	fmt.Println(os.Getenv("GO_ENV"))
@@ -152,4 +165,34 @@ func main() {
 	for k, v := range ma2 {
 		fmt.Printf("%v %v\n", k, v)
 	}
+
+	// struct
+	task1 := Task{
+		Title:    "Learn Golang",
+		Estimate: 3,
+	}
+	task1.Title = "Learn Go"
+	fmt.Printf("%[1]T %+[1]v %v\n", task1, task1.Title)
+
+	var task2 Task = task1
+	task2.Title = "new"
+	fmt.Printf("task1: %v task2: %v\n", task1.Title, task2.Title)
+
+	task1p := &Task{
+		Title:    "Learn concurrency",
+		Estimate: 2,
+	}
+	fmt.Printf("task1p: %T %+v %v\n", task1p, *task1p, unsafe.Sizeof(task1p))
+	// (*task1p).Title = "Changed"
+	task1p.Title = "Changed"
+	fmt.Printf("task1p: %+v\n", *task1p)
+	var task2p *Task = task1p
+	task2p.Title = "Changed by Task2"
+	fmt.Printf("task1: %+v\n", *task1p)
+	fmt.Printf("task2: %+v\n", *task2p)
+	task1.extendEstimate()
+	fmt.Printf("taks1 value receiver: %+v\n", task1.Estimate)
+	// (&task1).extendEstimatePointer()
+	task1.extendEstimatePointer()
+	fmt.Printf("taks1 value receiver: %+v\n", task1.Estimate)
 }
