b2436c6d87337b1f8184207e5b88a222c8844c11
keisuke-333 <keikubo.999@gmail.com>
2023-10-04 14:52:19 +0900

    basic channel usage

--- basic/concurrency-processing/main.go
+++ basic/concurrency-processing/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
+	"time"
 )
 
 func main() {
+	ch := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		fmt.Println("goroutine invoked")
+		ch <- 10
+		time.Sleep(500 * time.Millisecond)
 	}()
-	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	fmt.Println(<-ch)
 	wg.Wait()
-	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
-	fmt.Println("main func finished")
 }
