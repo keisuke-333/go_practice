d29106e88eec46bf6d9a1dab21b888c2d3a588c9
keisuke-333 <keikubo.999@gmail.com>
2023-10-04 14:56:08 +0900

    channel buffered

--- basic/concurrency-processing/main.go
+++ basic/concurrency-processing/main.go
@@ -7,14 +7,19 @@ import (
 )
 
 func main() {
-	ch := make(chan int)
+	ch1 := make(chan int)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		ch <- 10
+		ch1 <- 10
 		time.Sleep(500 * time.Millisecond)
 	}()
-	fmt.Println(<-ch)
+	fmt.Println(<-ch1)
 	wg.Wait()
+
+	ch2 := make(chan int, 1)
+	ch2 <- 1
+	// ch2 <- 2 // deadlock!
+	fmt.Println(<-ch2)
 }
