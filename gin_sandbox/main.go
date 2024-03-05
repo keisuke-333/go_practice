5ce49a18e1e589ecc46cb004f32cfab092cc50ff
keisuke-333 <keikubo.999@gmail.com>
2024-03-05 23:54:04 +0900

    create user auth

--- gin_sandbox/main.go
+++ gin_sandbox/main.go
@@ -39,5 +39,6 @@ func main() {
 	itemRouter.DELETE("/:id", itemController.Delete)
 
 	authRouter.POST("/signup", authController.Signup)
+	authRouter.POST("/login", authController.Login)
 	r.Run("localhost:8080")
 }
