e0c672ff3acd5ef51294f29d7e8180960d223ba0
keisuke-333 <keikubo.999@gmail.com>
2024-03-06 17:19:34 +0900

    add auth middleware

--- gin_sandbox/main.go
+++ gin_sandbox/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"sandbox/controllers"
 	"sandbox/infra"
+	"sandbox/middlewares"
+
 	// "sandbox/models"
 	"sandbox/repositories"
 	"sandbox/services"
@@ -30,13 +32,14 @@ func main() {
 
 	r := gin.Default()
 	itemRouter := r.Group("/items")
+	itemRouterWithAuth := r.Group("/items", middlewares.AuthMiddleware(authService))
 	authRouter := r.Group("/auth")
 
 	itemRouter.GET("", itemController.FindAll)
-	itemRouter.GET("/:id", itemController.FindById)
-	itemRouter.POST("", itemController.Create)
-	itemRouter.PUT("/:id", itemController.Update)
-	itemRouter.DELETE("/:id", itemController.Delete)
+	itemRouterWithAuth.GET("/:id", itemController.FindById)
+	itemRouterWithAuth.POST("", itemController.Create)
+	itemRouterWithAuth.PUT("/:id", itemController.Update)
+	itemRouterWithAuth.DELETE("/:id", itemController.Delete)
 
 	authRouter.POST("/signup", authController.Signup)
 	authRouter.POST("/login", authController.Login)
