3346303d06754196bd0e3999ca522d34403f40d1
keisuke-333 <keikubo.999@gmail.com>
2024-03-07 15:44:04 +0900

    create setupRouter

--- gin_sandbox/main.go
+++ gin_sandbox/main.go
@@ -4,18 +4,15 @@ import (
 	"sandbox/controllers"
 	"sandbox/infra"
 	"sandbox/middlewares"
-
 	"sandbox/repositories"
 	"sandbox/services"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func main() {
-	infra.Initialize()
-	db := infra.SetupDB()
-
+func setupRouter(db *gorm.DB) *gin.Engine {
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
@@ -26,17 +23,25 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.Default())
+
 	itemRouter := r.Group("/items")
 	itemRouterWithAuth := r.Group("/items", middlewares.AuthMiddleware(authService))
 	authRouter := r.Group("/auth")
-
 	itemRouter.GET("", itemController.FindAll)
 	itemRouterWithAuth.GET("/:id", itemController.FindById)
 	itemRouterWithAuth.POST("", itemController.Create)
 	itemRouterWithAuth.PUT("/:id", itemController.Update)
 	itemRouterWithAuth.DELETE("/:id", itemController.Delete)
-
 	authRouter.POST("/signup", authController.Signup)
 	authRouter.POST("/login", authController.Login)
+
+	return r
+}
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+	r := setupRouter(db)
+
 	r.Run("localhost:8080")
 }
